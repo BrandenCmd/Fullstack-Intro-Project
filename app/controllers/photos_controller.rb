51c56185c4cbf3c55ae086792be360a91d8d671c
BrandenCmd <branden@live.ca>
2020-10-22 20:25:36 -0500

    Basic pages for photos and photographers started

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,9 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.includes(:photographer).all
   end
 
   def show
+    @photo = Photo.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'photos/index'
-  get 'photos/show'
-  get 'photographers/index'
-  get 'photographers/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :photos, only: %i[index show]
+  resources :photographers, only: %i[index show]
 end
