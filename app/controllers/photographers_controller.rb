cb4df704802320a027171abf83ad42b04b69934f
BrandenCmd <branden@live.ca>
2020-10-23 23:38:29 -0500

    Photographers index now has a search field that work

--- app/controllers/photographers_controller.rb
+++ app/controllers/photographers_controller.rb
@@ -1,10 +1,14 @@
 class PhotographersController < ApplicationController
   def index
-    @photographers = Photographer.page(params[:page]).all
+    @photographers = Photographer.search(params[:search]).page(params[:page]).all
   end
 
   def show
     @photographer = Photographer.find(params[:id])
     @photos = Photo.where(photographer: @photographer.id).all
+  end
+
+  def photograper_params
+    params.require(:photographer).permit(:photographer, :search)
   end
 end
--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -1,4 +1,17 @@
 class Photographer < ApplicationRecord
   has_many :photos
   validates :photographer, presence: true, uniqueness: true
+
+  def self.search(search)
+    if search
+      photographer = Photographer.find_by(photographer: search)
+      if photographer
+        where(id: photographer)
+      else
+        Photographer.all
+      end
+    else
+      Photographer.all
+    end
+  end
 end
