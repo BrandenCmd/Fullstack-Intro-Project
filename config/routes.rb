a0ce6ba9ec8a6715c7cca1735d10123d8a77c6d0
BrandenCmd <branden@live.ca>
2020-10-23 22:34:44 -0500

    Some fixes and the addition of the home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :photos, only: %i[index show]
   resources :photographers, only: %i[index show]
   resources :about, only: %i[index]
